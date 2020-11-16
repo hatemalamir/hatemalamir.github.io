48129990c4ab9e9e87aad604ea8e5a8502fa859d
HatemAlamir90 <hatem.alamir.90@gmail.com>
2020-11-16 00:46:26 -0600

    ASCIImation: change speed and font size

--- ASCIImation_files/ascii.js
+++ ASCIImation_files/ascii.js
@@ -2,9 +2,23 @@ let whichOne = "Blank";
 let timer = null;
 let frames = null;
 let frame = 0;
+let intervalMS = 250;
+const fontSizes = {
+	"Tiny": "7pt",
+	"Small": "10pt",
+	"Medium": "12pt",
+	"Large": "16pt",
+	"Extra Large": "24pt",
+	"XXL" : "32pt"
+};
 
 (function () {
 	"use strict";
+	function animationFunc() {
+		document.getElementById("text-area").value = frames[frame];
+		frame = (frame + 1) % frames.length;
+	}
+
 	window.onload = function(){
 		// start button behavior
 		document.getElementById("start").onclick = () => {
@@ -12,11 +26,7 @@ let frame = 0;
 			document.getElementById("stop").disabled = false;
 			document.getElementById("animation").disabled = true;
 			frames = ANIMATIONS[whichOne].split("=====\n");
-
-			timer = window.setInterval(() => {
-				document.getElementById("text-area").value = frames[frame];
-				frame = (frame + 1) % frames.length;
-			}, 250);
+			timer = window.setInterval(animationFunc , intervalMS);
 		};
 		// stop button behavior
 		document.getElementById("stop").onclick = () => {
@@ -31,6 +41,21 @@ let frame = 0;
 		document.getElementById("animation").onchange = () => {
 			whichOne = document.getElementById("animation").value;
 			document.getElementById("text-area").value = ANIMATIONS[whichOne];
+		};
+		// turbo checkbox behavior
+		document.getElementById("turbo").onchange = () => {
+			intervalMS = intervalMS == 250 ? 50 : 250;
+			if(timer !== null) {
+				// we need to clear the previously running timer, if any, before setting a new one
+				// otherwise, both will be running
+				window.clearInterval(timer);
+				timer = window.setInterval(animationFunc , intervalMS);
+			}
+		};
+		// size button behavior
+		document.getElementById("fontsize").onchange = () => {
+			document.getElementById("text-area").style["fontSize"] =
+				fontSizes[document.getElementById("fontsize").value];
 		};
 	};
 })();
