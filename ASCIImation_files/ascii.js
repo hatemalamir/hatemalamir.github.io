62e2997d5fb5fd663e8fea3d4b16974e795f4224
HatemAlamir90 <hatem.alamir.90@gmail.com>
2020-11-16 00:23:42 -0600

    ASCIImation: get the animation running

--- ASCIImation_files/ascii.js
+++ ASCIImation_files/ascii.js
@@ -1,3 +1,8 @@
+let whichOne = "Blank";
+let timer = null;
+let frames = null;
+let frame = 0;
+
 (function () {
 	"use strict";
 	window.onload = function(){
@@ -6,12 +11,26 @@
 			document.getElementById("start").disabled = true;
 			document.getElementById("stop").disabled = false;
 			document.getElementById("animation").disabled = true;
+			frames = ANIMATIONS[whichOne].split("=====\n");
+
+			timer = window.setInterval(() => {
+				document.getElementById("text-area").value = frames[frame];
+				frame = (frame + 1) % frames.length;
+			}, 250);
 		};
 		// stop button behavior
 		document.getElementById("stop").onclick = () => {
 			document.getElementById("start").disabled = false;
 			document.getElementById("stop").disabled = true;
 			document.getElementById("animation").disabled = false;
+			document.getElementById("text-area").value = ANIMATIONS[whichOne];
+			window.clearInterval(timer);
+			timer = null;
+		};
+		// animation selection behavior
+		document.getElementById("animation").onchange = () => {
+			whichOne = document.getElementById("animation").value;
+			document.getElementById("text-area").value = ANIMATIONS[whichOne];
 		};
 	};
 })();
