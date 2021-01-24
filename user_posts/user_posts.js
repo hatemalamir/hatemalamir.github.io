c11776afcfbf38f76c6da984e4036dd59c9eb0b0
HatemAlamir90 <hatem.alamir.90@gmail.com>
2021-01-24 11:11:26 -0600

    User postst

---
+++ user_posts/user_posts.js
@@ -0,0 +1,39 @@
+$(function() {
+  $("#userIdBt").click(() => {
+    const userId = $("#userId").val();
+    if(userId)
+      $.get(
+        "http://jsonplaceholder.typicode.com/posts",
+        {"userId": userId}
+      ).done((data) => {
+        const posts= $("#posts");
+        posts.empty();
+        if(data.length < 1) {
+          posts.append($("<div>", {
+            "class": "noPosts",
+            "text": "No posts found!"
+          }));
+          return;
+        }
+        for(const p of data) {
+          const post = $("<div>", {"class": "post"});
+          post.append($("<div>", {
+            "class": "postTitle",
+            "text": "#" + p.id + ": " + p.title
+          }));
+          post.append($("<div>", {
+            "class": "postBody",
+            "text": p.body
+          }));
+          posts.append(post);
+        }
+      }).fail(() => {
+        const posts= $("#posts");
+        posts.empty();
+        posts.append($("<div>", {
+          "class": "error",
+          "text": "Some error occured wile reading posts!"
+        }));
+      });
+  });
+});
